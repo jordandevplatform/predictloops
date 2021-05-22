3049f618db088da81800856789046da02d02a277
Jordan Samsel <jordansamsel@Jordans-MacBook-Air.local>
2021-05-22 19:41:50 -0400

    first commit

---
+++ algoloops.js
@@ -0,0 +1,18 @@
+//for(var l=1; l<20; l+=2){console.log(l)}
+
+var numbers= [1,2,3,4,5]
+    var first=numbers[0]
+    var second=numbers[1]
+    var third=numbers[2]
+    var fourth=numbers[3]
+    var fifth=numbers[4]
+    console.log(first)
+    console.log(first)
+    console.log(second)
+    console.log(first + second)
+    console.log(third)
+    console.log(first + second + third)
+    console.log(fourth)
+    console.log(first + second + third + fourth)
+    console.log(fifth)
+    console.log(first + second + third + fourth + fifth)
