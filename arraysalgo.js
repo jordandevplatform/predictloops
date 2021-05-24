fbcc2690a204abc0519c25da83301fd397c1fb68
Jordan Samsel <jordansamsel@Jordans-MacBook-Air.local>
2021-05-24 12:32:26 -0400

    arrays algo

---
+++ arraysalgo.js
@@ -0,0 +1,14 @@
+/*var testArr = [6,3,5,1,2,4];
+var sum= 0
+for (var i=0; i<testArr.length; i++){
+    sum=sum+testArr[i]
+    console.log(testArr[i])
+    console.log(sum)
+}*/
+
+var arr = [6,3,5,1,2,4]
+var newArr= []
+for (var i=0; i<arr.length; i++){
+newArr=(arr[i]* i)
+console.log(newArr)
+}
